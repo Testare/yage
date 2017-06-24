f54b05bf7d3b642787b4cfba3a4cb55d8f1b1722
Testare <testare.i9z@gmail.com>
2017-06-23 22:33:59 -0700

    circle and vector collision data in its current representation is now drawn on the characters

--- src/babel/ui/draw-sprite.jsx
+++ src/babel/ui/draw-sprite.jsx
@@ -18,11 +18,48 @@ const spriteStyle = (props) => ({
     }
 )
 
+const DrawCollisionFrame = ({player}) => (
+    <svg>
+        {
+            pAnimation(player).frames[player.currentFrame].collisionData.map(
+                (collisionFrame, i) => {
+                    coords = collisionFrame.coords.split(",")
+                    switch(collisionFrame.shape) {
+                        case "circle":
+                            return <circle
+                                key={i}
+                                cx={coords[0]}
+                                cy={coords[1]}
+                                r={coords[2]}
+                                stroke="blue"
+                                fill="none"
+                            />
+                        case "vector":
+                            return <line
+                                key={i}
+                                x1={coords[0]}
+                                y1={coords[1]}
+                                x2={coords[2]}
+                                y2={coords[3]}
+                                stroke="blue"
+                                fill="none"
+                            />
+                        default:
+                    }
+
+                }
+            )
+        }
+    </svg>
+
+)
+
 const DrawSprite = props => (
     <span
         className="drawsprite"
         style={spriteStyle(props)}
     >
+    <DrawCollisionFrame {...props} />
     </span>
 )
 
